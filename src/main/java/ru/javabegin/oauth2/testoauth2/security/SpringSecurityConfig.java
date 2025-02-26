c3e159f13a569950b05ac1f3d93cc8f8bd1c0b6d
MaximKiel <loup.du.nord@yandex.ru>
2025-02-26 14:17:17 +0300

    Create and implement KCRoleConverter and add into SpringSecurityConfig

--- src/main/java/ru/javabegin/oauth2/testoauth2/security/SpringSecurityConfig.java
+++ src/main/java/ru/javabegin/oauth2/testoauth2/security/SpringSecurityConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.javabegin.oauth2.testoauth2.converter.KCRoleConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -14,10 +16,15 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KCRoleConverter());
+
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/test/login").anonymous()
-                .anyRequest().authenticated()
-        );
+                .anyRequest().authenticated())
+                .oauth2ResourceServer(configurer -> configurer
+                        .jwt(oauth -> oauth.jwtAuthenticationConverter(jwtAuthenticationConverter)));
         return http.build();
     }
 }
